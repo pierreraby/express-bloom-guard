6a5e1d048a1e097b22e06f52e7aabd1d90f51a3e
pierreraby <pierre@pierreraby.dev>
2024-12-30 16:30:49 +0100

    feat: enhance benchmarking with false positive rate calculation and adjustable nanoid length

--- benchmarks.js
+++ benchmarks.js
@@ -21,15 +21,17 @@ function benchJWTVerify() {
   console.log(`Average time taken to verify token: ${(endTime - startTime) / iterations} milliseconds`);
 }
 
-function generateClaims(NUM_ITEMS) {
+function generateClaims(NUM_ITEMS,nanoidLength = 21) {
   console.log('Generating claims...');
   const claims = [];
 
   for (let i = 0; i < NUM_ITEMS; i++) {
-    claims.push('jti-' + nanoid());
+    claims.push('jti-' + nanoid(nanoidLength));
   }
   console.log('Claims generated : ' + claims.length);
-  console.log('Claims generated : ' + claims[500000]);
+  // console.log('Claims generated Number 0: ' + claims[0]);
+  // console.log('Claims generated Number 500 000: ' + claims[500000]);
+  // console.log('Claims generated Number 999 999: ' + claims[999999]);
   return claims;
 }
 
@@ -44,6 +46,7 @@ function benchFilterAdd(claims) {
   }
   endTime = performance.now();
 
+  console.log(`filterAdd adds to current and next filters !`);
   console.log(`Time taken to add to filter ${iterations} times: ${endTime - startTime} milliseconds`);
   console.log(`Average time taken to add to filter: ${(endTime - startTime) / iterations} milliseconds`);
 }
@@ -59,8 +62,26 @@ function benchFilterHas(claims) {
   }
   endTime = performance.now();
 
+  console.log(`filterHas check previous and current filters !`);
   console.log(`Time taken to check filter ${iterations} times: ${endTime - startTime} milliseconds`);
   console.log(`Average time taken to check filter: ${(endTime - startTime) / iterations} milliseconds`);
+}
+
+function benchFalsePositiveRate() {
+  const NUM_ITEMS = 1000000;
+  const iterations = 1000;
+  const falsePositives = 0;
+
+  for (let i = 0; i < iterations; i++) {
+    const claims = generateClaims(NUM_ITEMS, 32);
+    for (const claim of claims) {
+      filterHas(claim) && falsePositives++;
+    }
+    console.log('iteration: ' + i);
+    console.log(`False positive with ${NUM_ITEMS} tokens : ${falsePositives}`);
+  }
+  console.log(`False positive rate with 1 billion tokens : ${falsePositives / (NUM_ITEMS * iterations)}`);
+  console.log(`False positives with 1 billion tokens : ${falsePositives}`);
 }
 
 function benchBloomFilter() {
@@ -70,7 +91,10 @@ function benchBloomFilter() {
   benchFilterHas(claims);
 }
 
+
+
 benchJWTVerify();
 benchBloomFilter();
+//benchFalsePositiveRate(); // This will run indefinitely
 
 exit(0); // Exit the process because filter rotation is on a timer
